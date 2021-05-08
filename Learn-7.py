8ff5867068d01f6ff5b61fb48a1379406d4df9e0
Deepesh Sherwal <deepeshsherwal@gmail.com>
2021-05-08 15:16:14 +0530

    7 - Color Detection

--- Learn-7.py
+++ Learn-7.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+def empty(a):
+    pass
 
 def stackImages(scale,imgArray):
     rows = len(imgArray)
@@ -33,16 +35,48 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-img = cv2.imread('Resources/lena.png')
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-imgStack = stackImages(0.5,([img,imgGray,img],[img,img,img]))
+path = "Resource/lambo.png"
 
-# imgHor = np.hstack((img,img))
-# imgVer = np.vstack((img,img))
-#
-# cv2.imshow("Horizontal",imgHor)
-# cv2.imshow("Vertical",imgVer)
-cv2.imshow("ImageStack",imgStack)
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
 
-cv2.waitKey(0)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",154,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+
+while True:
+    img = cv2.imread(path)
+
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+
+    mask = cv2.inRange(imgHSV,lower,upper)
+
+    imgResult = cv2.bitwise_and(img,img,mask = mask)
+
+    stackedImage = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
+
+    # cv2.imshow("Orginal", img)
+
+    # cv2.imshow("HSV", imgHSV)
+
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("New Image", imgResult)
+    cv2.imshow("Stacked Image", stackedImage)
+
+    cv2.waitKey(1)
